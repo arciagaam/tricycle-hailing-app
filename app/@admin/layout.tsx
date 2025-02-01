852d7847eb2c8adc51e3e8d9589e01f87d80edc9
Miguel Arciaga <alberto.arciaga@kmc.solutions>
2025-02-01 23:52:00 +0800

    updates

--- app/@admin/layout.tsx
+++ app/@admin/layout.tsx
@@ -3,10 +3,10 @@ import AdminMobileMenu from './_components/AdminMobileMenu'
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
     return (
-        <div className="flex flex-col">
+        <div className="flex flex-col h-screen">
             <AdminMobileMenu />
 
-            <div className="flex flex-col p-4">
+            <div className="flex flex-col p-4 h-full">
                 {children}
             </div>
         </div>
