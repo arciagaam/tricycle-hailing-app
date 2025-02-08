72b061a4af465b0fcd6357d32700d41d7ceeee5b
Miguel <miguelarciagaa@gmail.com>
2025-02-08 22:55:47 +0800

    updates

--- app/@driver/_components/DriverCurrentBooking.tsx
+++ app/@driver/_components/DriverCurrentBooking.tsx
@@ -1,5 +1,6 @@
 import GoogleMapsDirections from '@/components/google-maps/GoogleMapsDirections'
 import { Button } from '@/components/ui/button'
+import { Drawer, DrawerContent, DrawerFooter, DrawerHeader, DrawerTitle, DrawerTrigger } from '@/components/ui/drawer'
 import { ResponsiveProvider } from '@/hooks/useResponsive'
 import { BookingWithRelations } from '@/lib/types'
 import { socket } from '@/socket'
@@ -44,23 +45,42 @@ export default function DriverCurrentBooking({ booking }: { booking: BookingWith
                 <GoogleMapsDirections destination={{ lat: parseFloat(booking.dropoff.latitude), lng: parseFloat(booking.dropoff.longitude) }} />
             </ResponsiveProvider>
 
-            <div className='flex flex-col w-[92dvw] h-fit absolute left-[4vw] bottom-[4dvh]'>
-                <div className="w-full flex flex-col bg-background rounded-md p-4 gap-5">
-                    <p>Ride Details</p>
-                    <p>Dropoff: <span>{booking.dropoff.address}</span></p>
-                    <p>Fare: <span>P50.00</span></p>
-                    <hr />
-                    <p>Passenger Details</p>
-                    <p>Rider: <span>{booking.passenger.firstName}</span></p>
 
-                    {
-                        booking.status == 'ACCEPTED' && <Button onClick={pickupPassenger}>Pickup Passenger</Button>
-                    }
 
-                    {
-                        booking.status == 'ONGOING' && <Button onClick={dropoffPassenger}>Dropoff Passenger</Button>
-                    }
-                </div>
+            <div className='flex flex-col w-full h-fit absolute bottom-0'>
+                <Drawer>
+                    <DrawerTrigger asChild>
+
+                        <Button className='flex flex-col h-fit py-6 rounded-b-none'>
+                            <p>ANIMATION</p>
+                            <p>Click to view details</p>
+                        </Button>
+                    </DrawerTrigger>
+                    <DrawerContent>
+                        <DrawerHeader>
+                            <DrawerTitle>Current Booking</DrawerTitle>
+                        </DrawerHeader>
+
+                        <div className="w-full flex flex-col bg-background rounded-md p-4 gap-5">
+                            <p>Ride Details</p>
+                            <p>Dropoff: <span>{booking.dropoff.address}</span></p>
+                            <p>Fare: <span>P50.00</span></p>
+                            <hr />
+                            <p>Passenger Details</p>
+                            <p>Rider: <span>{booking.passenger.firstName}</span></p>
+
+                            {
+                                booking.status == 'ACCEPTED' && <Button onClick={pickupPassenger}>Pickup Passenger</Button>
+                            }
+
+                            {
+                                booking.status == 'ONGOING' && <Button onClick={dropoffPassenger}>Dropoff Passenger</Button>
+                            }
+                        </div>
+                    </DrawerContent>
+                </Drawer>
+
+
             </div>
         </div>
     )
