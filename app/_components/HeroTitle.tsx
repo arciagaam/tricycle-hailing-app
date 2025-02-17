cad34cc540dff18f9d27a6033434344a4bbaf45a
allenJcodes <110220645+allenJcodes@users.noreply.github.com>
2025-02-18 00:55:30 +0800

    logo sample

--- app/_components/HeroTitle.tsx
+++ app/_components/HeroTitle.tsx
@@ -1,11 +1,11 @@
+import Image from 'next/image'
 import React from 'react'
 import { } from 'react-icons'
-import { FaMotorcycle } from 'react-icons/fa'
 
 export default function HeroTitle() {
     return (
         <div className='flex flex-col gap-2 w-full items-center justify-center'>
-            <FaMotorcycle className='text-4xl font bold tracking-tighter leading-none text-primary' />
+            <Image src="/logo.png" alt="logo" width={40} height={40} unoptimized/>
             <h1 className='text-primary font-black text-2xl tracking-tighter leading-none'>
                 SBU TodaTracker
             </h1>
